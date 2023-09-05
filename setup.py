9d8138291b0ff80135f9de3ef056c627c18a83e1
Andrei Anufriev <andrey.anufriev@intel.com>
2023-09-05 16:09:55 +0200

    Initial commit for OVModelForCausalLM support.

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setuptools.setup(
         "transformers>=4.1",
         "zstandard",
         "accelerate>=0.17.1",
+        "optimum-intel>=1.10.1"
     ],
     extras_require={
         "dev": ["black", "flake8", "pre-commit", "pytest", "pytest-cov"],
